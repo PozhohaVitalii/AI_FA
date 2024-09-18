c812b8bfc4e2a0ec9327988fe8fbe2b121d1676d
PozhohaVitalii <112852134+PozhohaVitalii@users.noreply.github.com>
2024-09-18 14:40:24 +0300

    Add project files.

---
+++ AI_FA/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace AI_FA
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            richTextBox1 = new RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(232, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(646, 545);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(12, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(214, 43);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "Sectors count";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox1.TextChanged += textBox1_TextChanged;
+            textBox1.MouseEnter += textBox1_MouseEnter;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 178);
+            button1.Name = "button1";
+            button1.Size = new Size(214, 62);
+            button1.TabIndex = 2;
+            button1.Text = "ADD image";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 246);
+            button2.Name = "button2";
+            button2.Size = new Size(214, 60);
+            button2.TabIndex = 3;
+            button2.Text = "Tranform";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(12, 312);
+            button3.Name = "button3";
+            button3.Size = new Size(214, 60);
+            button3.TabIndex = 4;
+            button3.Text = "CALC. properties";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(897, 12);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(197, 529);
+            richTextBox1.TabIndex = 5;
+            richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1117, 569);
+            Controls.Add(richTextBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private OpenFileDialog openFileDialog1;
+        private RichTextBox richTextBox1;
+    }
+}
---
+++ AI_FA/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace AI_FA
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap ImageExempl;
+        public Bitmap BlackWhite;
+        Color[,] color;
+        public static int SectorsCount;
+        Class2 BlackBox;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_MouseEnter(object sender, EventArgs e)
+        {
+            textBox1.Text = String.Empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image = Image.FromFile(openFileDialog.FileName);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                    ImageExempl = new Bitmap(pictureBox1.Image);
+                    BlackWhite = new Bitmap(pictureBox1.Image);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int minj = 0;
+            int mini = 0;
+            int maxj = 0;
+            int maxi1 = 0;
+
+            try
+            {
+                SectorsCount = int.Parse(textBox1.Text);
+            }
+            catch {
+                textBox1.Text = "input count first";
+            }
+            if (SectorsCount != 0)
+            {
+                double sectorAngle = (double)90.0 / SectorsCount;
+                BlackBox = new Class2();
+                BlackBox.Add(ImageExempl);
+                int CountOfBlackPoints = 0;
+                color = GetPixelArray(ImageExempl);
+                mini = color.GetLength(0);
+                minj = color.GetLength(1);
+
+                for (int i = 0; i < color.GetLength(0); i++)
+                {
+                    for (int j = 0; j < color.GetLength(1); j++)
+                    {
+                        int gray = (int)(0.299 * color[i, j].R + 0.587 * color[i, j].G + 0.114 * color[i, j].B);
+                        if (gray > 128)
+                        {
+                            gray = 255;
+                        }
+                        else if (gray <= 128)
+                        {
+                            gray = 1;
+                            CountOfBlackPoints++;
+                            mini = mini < i ? mini : i;
+                            minj = minj < j ? minj : j;
+                            maxi1 = maxi1 > i ? maxi1 : i;
+                            maxj = maxj > j ? maxj : j;
+
+                        }
+                        double hipotenusa = Math.Sqrt(Math.Pow( i, 2) + Math.Pow(color.GetLength(1) - j, 2));
+                        double angle = Math.Asin((double)i / hipotenusa) * (180.0 / Math.PI);
+                        int n = 0;
+                        do
+                        {
+                            Console.WriteLine(sectorAngle + " " + n);
+                            n++;
+                        }
+                        while (angle > n * sectorAngle);
+                        if (gray == 255)
+                        {
+                            gray = gray - n * 7;
+                        }
+                       
+                        BlackWhite.SetPixel(i, j, Color.FromArgb(255, gray, gray, gray));
+                    }
+                }
+                textBox1.Text = sectorAngle.ToString("F4");
+                richTextBox1.Text += "Count of black points: " + CountOfBlackPoints.ToString() + "\n";
+                using (Graphics g = Graphics.FromImage(BlackWhite))
+                {                   
+                    Pen pen = new Pen(Color.Red, 3);
+                    g.DrawRectangle(pen, mini, minj, maxi1, maxj);                    
+                    pen.Dispose();
+                }
+                pictureBox1.Image = BlackWhite;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+        }
+       
+
+        public static Color[,] GetPixelArray(Bitmap imageExempl)
+        {
+            // Створення двовимірного масиву для пікселів
+            int width = imageExempl.Width;
+            int height = imageExempl.Height;
+            Color[,] pixelArray = new Color[width, height];
+
+            // Блокування бітів зображення
+            Rectangle rect = new Rectangle(0, 0, width, height);
+            BitmapData bitmapData = imageExempl.LockBits(rect, ImageLockMode.ReadOnly, imageExempl.PixelFormat);
+
+            try
+            {
+                // Визначення кількості байтів на один рядок
+                int bytesPerPixel = Image.GetPixelFormatSize(imageExempl.PixelFormat) / 8;
+                int byteCount = bitmapData.Stride * height;
+                byte[] pixels = new byte[byteCount];
+
+                // Копіювання піксельних даних у масив
+                Marshal.Copy(bitmapData.Scan0, pixels, 0, byteCount);
+
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        // Обчислення індексу в масиві для конкретного пікселя
+                        int index = (y * bitmapData.Stride) + (x * bytesPerPixel);
+
+                        // Зчитування компонентів кольору
+                        byte blue = pixels[index];
+                        byte green = pixels[index + 1];
+                        byte red = pixels[index + 2];
+
+                        // Якщо формат зображення підтримує альфа-канал
+                        byte alpha = bytesPerPixel == 4 ? pixels[index + 3] : (byte)255;
+
+                        // Додавання кольору до двовимірного масиву
+                        pixelArray[x, y] = Color.FromArgb(alpha, red, green, blue);
+                    }
+                }
+            }
+            finally
+            {
+                // Розблокування бітів зображення
+                imageExempl.UnlockBits(bitmapData);
+            }
+
+            return pixelArray;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            BlackBox.calcFirst();
+            int[,] SignsVector = BlackBox.getSector();
+            double[] FedoryshinAndriyM1 = new double[SignsVector.GetLength(1)];
+            double[] FedoryshinAndriyS1 = new double[SignsVector.GetLength(1)];
+            richTextBox1.Text = richTextBox1.Text + "\n";
+            richTextBox1.Text = richTextBox1.Text + "Absolute vector" + "\n";
+
+            for (int i = 0; i < SignsVector.GetLength(0); i++)
+            {
+               
+                for (int j = 0; j < SignsVector.GetLength(1); j++)
+                {
+                    FedoryshinAndriyM1[j] = SignsVector[i, j];
+                    FedoryshinAndriyS1[j] = SignsVector[i, j];
+                    richTextBox1.Text = richTextBox1.Text + " " + SignsVector[i, j];
+                }
+                richTextBox1.Text = richTextBox1.Text + "\n";
+            }
+            double mx = FedoryshinAndriyM1.Max();
+            for (int i = 0; i < FedoryshinAndriyM1.Length; i++)
+            {
+
+                FedoryshinAndriyM1[i] = FedoryshinAndriyM1[i] / mx;
+                FedoryshinAndriyS1[i] = FedoryshinAndriyS1[i] / BlackBox.CountOfBlackPoints;
+            }
+            richTextBox1.Text = richTextBox1.Text + "FedoryshinAndriyS1" + "\n" ;
+            for (int i = 0; i < FedoryshinAndriyS1.Length; i++)
+            {
+                richTextBox1.Text = richTextBox1.Text + FedoryshinAndriyS1[i].ToString("F2") + "  ";
+            }
+            richTextBox1.Text = richTextBox1.Text + "FedoryshinAndriyM1" + "\n";
+            for (int i = 0; i < FedoryshinAndriyM1.Length; i++)
+            {
+                richTextBox1.Text = richTextBox1.Text + FedoryshinAndriyM1[i].ToString("F2") + "  ";
+            }
+
+        }
+    }
+}
